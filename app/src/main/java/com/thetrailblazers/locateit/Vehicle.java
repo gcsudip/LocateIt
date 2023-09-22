f0fbd4d41c12fb3547ac5d1504a7ec1ec9420587
Kunj Mandal <kunjmandal3432@gmail.com>
2023-09-22 15:04:11 +0545

    Implemented Recycler View with Firebase

---
+++ app/src/main/java/com/thetrailblazers/locateit/Vehicle.java
@@ -0,0 +1,17 @@
+package com.thetrailblazers.locateit;
+
+public class Vehicle {
+    String name, code, imgUrl;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+}
--- app/src/main/java/com/thetrailblazers/locateit/ui/home/HomeFragment.java
+++ app/src/main/java/com/thetrailblazers/locateit/ui/home/HomeFragment.java
@@ -4,29 +4,56 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.thetrailblazers.locateit.Vehicle;
+import com.thetrailblazers.locateit.VehicleAdapter;
 import com.thetrailblazers.locateit.databinding.FragmentHomeBinding;
+
+import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
-
+    DatabaseReference databaseReference;
+    VehicleAdapter vehicleAdapter;
+    ArrayList<Vehicle> list;
+    RecyclerView recyclerView;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-
         binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        recyclerView = binding.vehicleRecyclerView;
+        databaseReference = FirebaseDatabase.getInstance().getReference("Vehicles");
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        list = new ArrayList<>();
+        vehicleAdapter = new VehicleAdapter(getContext(), list);
+        recyclerView.setAdapter(vehicleAdapter);
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dataSnapshot: snapshot.getChildren()) {
+                    Vehicle vehicle = dataSnapshot.getValue(Vehicle.class);
+                    list.add(vehicle);
+                }
+                vehicleAdapter.notifyDataSetChanged();
+            }
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        return binding.getRoot();
     }
 
     @Override
